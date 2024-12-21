19e94c9ab5a178a4302a6661e6d5e0fec1835fb4
Eagle <110081590+eaglehuntt@users.noreply.github.com>
2024-12-21 23:11:13 +0000

    Added monthly profit calculator

--- src/modules/etsy.py
+++ src/modules/etsy.py
@@ -5,6 +5,7 @@ import pandas as pd
 class Etsy:
     def __init__(self):
         self.data = self.load_data()
+        self.months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
     def load_data(self):
         # Path to the 'etsy' folder
@@ -17,26 +18,34 @@ class Etsy:
         df = pd.concat(map(pd.read_csv, joined_list), ignore_index=True) 
         return df
 
-    def get_net_profit(self):
-        # Get all entries in "Net" column
-        net = self.data["Net"]
+    def get_net_profit_total(self):
+       net_profit_total = 0
+       
+       for i in range(12):
+           net_profit_total += self.get_net_profit_by_month(i + 1)    
+           
+       return net_profit_total
+    
+    def get_net_profit_by_month(self, month:int):
 
-        # Replace the random -- with a pandas NA
-        net = net.replace("--", pd.NA)
+        # Get all transactions from specified month
+        month_net_profit = self.data[self.data["Date"].str.contains(self.months[month - 1])]["Net"]
+
+        # Replace the -- with a pandas NA
+        month_net_profit = month_net_profit.replace("--", pd.NA)
 
         # Replace all $ with nothing
-        net = net.str.replace("[$]", "", regex=True)
+        month_net_profit = month_net_profit.str.replace("[$]", "", regex=True)
 
         # Fill the pandas NA with 0s
-        net = net.fillna(0)
+        month_net_profit = month_net_profit.fillna(0)
 
         # Convert the data type from string to float
-        net = net.astype(float)
+        month_net_profit = month_net_profit.astype(float)
 
         # Calcluate the net profit by getting the sum of all entries in the net column
-        return net.sum()
-
+        return month_net_profit.sum()
 
 # USAGE: 
 # etsy = Etsy()
-# print(etsy.get_net_profit())
+# print(etsy.get_net_profit_total())
